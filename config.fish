c514918c23c90ff3a36109c9227947ffd6f1abc2
Tobias Hort <mail@tobiashort.com>
2024-10-16 07:20:47 +0200

    initial commit

---
+++ config.fish
@@ -0,0 +1,30 @@
+# Remove greeting
+set fish_greeting
+
+# Prompt style
+function fish_prompt
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_showstashstate 1
+    set -l symbol ' $ '
+    if fish_is_root_user
+        set symbol ' # '
+    end
+    echo -n $USER@$hostname
+    echo -n (prompt_pwd)
+    echo -n (fish_git_prompt)
+    echo -n $symbol
+end
+
+# Use vi bindings
+fish_vi_key_bindings
+
+# Use different cursor shapes for different vi modes
+set fish_cursor_default block
+set fish_cursor_insert line
+set fish_cursor_replace_one underscore
+set fish_cursor_visual block
+
+# Remove vi mode indicator from prompt
+function fish_mode_prompt
+end
