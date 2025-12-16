82932bc91882604ce78b000f2652405cfc1f9025
Tobias Hort <tobias.hort-giess@compass-security.com>
2025-12-16 09:02:47 +0100

    improve sort behaviour, remove invalid selections

--- functions/j.fish
+++ functions/j.fish
@@ -8,12 +8,13 @@ function j --description "Fuzzy cd into a directory from ~/.cache/cd_history"
     end
 
     # Use fzf to select a directory
-    set selection (FZF_DEFAULT_OPTS="--bind tab:down,shift-tab:up" fzf --reverse --no-sort --height 10 < $cache_file)
+    set selection (FZF_DEFAULT_OPTS="--bind tab:down,shift-tab:up" fzf --reverse --height=10 --scheme=path < $cache_file)
 
     if test -n "$selection" -a -d "$selection"
         cd "$selection"
     else
         echo "Invalid or no selection." >&2
+        grep -vFx -- "$selection" $cache_file | sponge $cache_file
         return 1
     end
 end
