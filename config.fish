fc90d8a475e2ab9d87a151bb35bb9fbf9e4f00f3
Tobias Hort <mail@tobiashort.com>
2026-07-02 22:20:42 +0200

    custom helix like bindings

--- config.fish
+++ config.fish
@@ -39,17 +39,11 @@ function postexec --on-event fish_postexec
     set CMD_DURATION 0
 end
 
-# Use vi bindings
-fish_vi_key_bindings
+# Custom helix-like key bindings
+fish_th_key_bindings
+
 # MacOS, if highlighting in visual mode does not work:
 # set -g fish_color_selection --background=brblue --bold
-
-# Use different cursor shapes for different vi modes
-# set -g fish_vi_force_cursor 1
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
-set fish_cursor_visual block
 
 # Remove vi mode indicator from prompt
 function fish_mode_prompt
